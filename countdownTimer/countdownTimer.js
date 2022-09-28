6b17970842f014a49093aea23c83833896ff0e75
ImranAhmed27 <imranshaik027@gmail.com>
2022-09-28 15:02:16 +0530

    added new projects

--- countdownTimer/countdownTimer.js
+++ countdownTimer/countdownTimer.js
@@ -1,30 +1,32 @@
-const daysEl = document.getElementById('days')
-const hoursEl = document.getElementById('hours')
-const minsEl = document.getElementById('mins')
-const secondsEl = document.getElementById('seconds')
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
-const newYears = '1 Jan 2022'
+const newYears = "1 Jan 2021";
 
-function countdown (){
-    const newYearDate = new Date (newYears)
-    const currentDate = new Date()
-    const totalSeconds = (newYearDate - currentDate)/1000;
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
 
-    const days = Math.floor(totalSeconds/3600/24);
-    const hours = Math.floor(totalSeconds/3600) % 24;
-    const mins = Math.floor(totalSeconds/60) % 60;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    daysEl.innerHTML = days
-    hoursEl.innerHTML = formatTime(hours)
-    minsEl.innerHTML = formatTime(mins)
-    secondsEl.innerHTML = formatTime(seconds)
-
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
 
-function formatTime (time) {
-    return time < 10 ? `0${time}`: time;
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
-countdown()
-setInterval(countdown,1000)
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
